7b44e0acba10209695ad914a8067f8d50861f152
Keith Fahlgren <abdelazer@gmail.com>
2013-11-20 11:55:27 -0800

    Toy app basically working

--- api/manage_users.js
+++ api/manage_users.js
@@ -2,28 +2,78 @@ var port = 8005;
 var express = require('express');
 var app = express();
 
+app.configure(function(){
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+});
+
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-// url(r'^revoke-access/$', 'revoke_user_access', name='revoke_user_access'),
-// url(r'^tutorial-progress/(?P<user_pk>\d+)/$', 'user_tutorial_progress', name='user_tutorial_progress'),
+app.post(/revoke-access/, function(req, res) {
+  var uid = req.body.uid;
+  var data = null;
+  if (uid === "4") {
+    res.send(200, 'Revoked access for ' + uid);
+  }
+  else {
+    res.send(404, 'User not found');
+  }
+});
+
 app.get('/tutorial-progress/:uid?', function(req, res) {
   var uid = req.params.uid;
-  var data = [{"completed": true,
-               "finished_time": "2013-10-09", 
-               "num_completed_lessons": 12,
-               "num_lessons": 12, 
-               "started_time": "2013-10-09", 
-               "title": "Developing Others", 
-               "url": "/tutorials/developing-others"}];
+  var data = null;
 
-  res.json(data);
+  if (uid === "4") {
+    data = [{"num_lessons": 12, 
+             "finished_time": "", 
+             "title": "Developing Others", 
+             "url": "/tutorials/developing-others", 
+             "started_time": "2013-10-09", 
+             "completed": false, 
+             "num_completed_lessons": 2}];
+  }
+  else if (uid === "5") {
+    data = [{"num_lessons": 16, 
+             "finished_time": "", 
+             "title": "Collaboration", 
+             "url": "/tutorials/collaboration", 
+             "started_time": "2013-10-07", 
+             "completed": false,
+             "num_completed_lessons": 0}, 
+            {"num_lessons": 15, 
+             "finished_time": "2013-10-16",
+             "title": "Building Teams", 
+             "url": "/tutorials/building-teams", 
+             "started_time": "2013-10-02", 
+             "completed": true, 
+             "num_completed_lessons": 15}, 
+            {"num_lessons": 12, 
+             "finished_time": "", 
+             "title": "Developing Others", 
+             "url": "/tutorials/developing-others", 
+             "started_time": "2013-10-07", 
+             "completed": false, 
+             "num_completed_lessons": 0}, 
+            {"num_lessons": 23, 
+             "finished_time": "",
+             "title": "Change Management", 
+             "url": "/tutorials/change-management",
+             "started_time": "2013-10-02", 
+             "completed": false, 
+             "num_completed_lessons": 0}];
+  }
+
+  if (data) {
+    res.json(data);
+  }
+  else {
+    res.send(404, 'User not found');
+  }
 });
 
-app.configure(function(){
-  app.use(express.logger('dev'));
-});
 
 app.listen(port);
 
